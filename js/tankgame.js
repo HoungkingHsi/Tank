e5510e5075cc55761c7b79bcbc30becaf972b241
Houngking Hsi <HoungkingHsi@outlook.com>
2016-03-08 23:04:04 +0800

    add js files,lib,css files

---
+++ js/tankgame.js
@@ -0,0 +1,33 @@
+/**
+ * Created by houngking on 16/3/8.
+ */
+"use strict"
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+
+canvas.style.border = "1px solid black";
+
+init();
+
+function init(){
+    initCanvas();
+    initScene();
+    initImage();
+    initSound();
+}
+
+function initCanvas(){
+
+}
+
+function initScene(){
+
+}
+
+function initImage(){
+
+}
+
+function initSound(){
+
+}
